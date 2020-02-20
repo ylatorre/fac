406e848443a8fe7967cc87ef6070f735e2a7ceda
Yvan LATORRE <32170221+ylatorre@users.noreply.github.com>
2020-02-20 19:47:34 +0100

    Add files via upload

---
+++ S2/outils-info/tp4/volume.py
@@ -0,0 +1,36 @@
+# 1) imports
+
+
+
+
+
+# 2) définitions de fonctions
+
+
+
+
+### NE PAS MODIFIER CETTE FONCTION ###
+def verifie_presque_egal(a, b):
+    if round(a, 5) != round(b, 5):
+        raise ValueError("ERREUR : «"+str(a)+"» != «"+str(b)+"»")
+######################################
+
+
+    
+# 3 et 4) programme principal
+
+### Dé-commenter les tests suivants, petit à petit
+### au fur et à mesure que vous écrivez les fonctions
+
+#verifie_presque_egal( carre(8), 64)
+#verifie_presque_egal( carre(-10), 100)
+#verifie_presque_egal( cube(10), 1000)
+#verifie_presque_egal( cube(-0.1), -0.001)
+#verifie_presque_egal( logbase(1024, 2), 10)
+#verifie_presque_egal( logbase(1024, 1024), 1)
+#verifie_presque_egal( aire_disque(2.5), 19.634954084936208)
+#verifie_presque_egal( volume_cylindre(2.5, 100), 1963.4954084936208)
+
+### Ajouter vos propres tests ci-dessous
+
+
