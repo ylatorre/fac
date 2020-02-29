5a7c31326eb6064bc25b5884b989e84f15560444
ylatorre <yvanlatorre@hotmail.fr>
2020-02-29 21:40:30 +0100

    magic 1

---
+++ S2/outils-info/tp6/magic-apir.py
@@ -0,0 +1,23 @@
+from qtido import *
+import sys
+
+
+def cercle_(r):
+    f = creer(600,600)
+    i = 0
+    j = 0
+    l = 0
+    while i == 0:
+        if r > j:
+            l = l + 20
+            couleur(f,0,0,1)
+            cercle(f,300,300,l)
+            j = j +1
+        else:
+            if r <= j:
+                couleur(f,1,0,0)
+                cercle(f,300,300,l+20)
+                i = 1
+
+    attendre_fermeture(f)
+cercle_(int(sys.argv[1]))
